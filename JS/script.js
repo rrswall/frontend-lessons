3e7c62d9760578517787a0ecc165f0f4048e895b
rrswall <zergerroya01@gmail.com>
2026-04-04 03:07:49 +0400

    js first task uploading

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+let num = 13;
+num = num ** 5;
+num = num % 3;
+console.log(num)
+
+let num1 = 24;
+console.log(num1 ** 5 % 3)
+
+let firstName = "Roia";
+let lastName = "Zargarova";
+console.log("Sizin adınız " + firstName + " " + lastName + ". Tanışlığımıza çox şadam!")
+
+
+let firstName1 = "Rashad";
+let lastName1 = "Mammadov";
+let greeting = `Sizi bir daha görməyimizə şadıq, ${firstName1} ${lastName1}.`;
+console.log(greeting)
+
+
+const fullName = 'Roia Zargarova';
+console.log(fullName.replace(" ", "").length)
